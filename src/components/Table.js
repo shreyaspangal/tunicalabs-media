78972bbfc4933bc239816151f413173a9fc6543d
Shreyas <shreyaspangal03@gmail.com>
2022-07-03 17:04:45 +0530

    Update table component as parent with new state variables
    - comment out mui table pagination
    - add edit & read only table rows as child components

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,4 @@
-import * as React from "react";
-import Paper from "@mui/material/Paper";
+import React, { useState, useEffect } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -7,13 +6,100 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
+// Redirection
 import { Link } from "react-router-dom";
-import { columns, rows } from "../DB/TableData";
+// Data
+import { columns, rows } from "../db/TableData";
+// Import Rows
+import ReadOnlyRow from "./ReadOnlyRow";
+import EditableRow from "./EditableRow";
 
 
-export default function ColumnGroupingTable() {
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+export default function ColumnGroupingTable({ tableData, setTableData }) {
+
+    // ReadOnly Mode State
+    const [editStudentId, setEditStudentId] = useState(null);
+
+    // Edit Mode State
+    const [editFormData, setEditFormData] = useState({
+        name: "",
+        age: "",
+        school: "",
+        classes: "",
+        division: "",
+        status: "",
+    });
+
+    // ReadOnly Mode Method
+    const handleEditClick = (event, row) => {
+        event.preventDefault();
+        setEditStudentId(row.id);
+
+        const formValues = {
+            name: row.name,
+            age: row.age,
+            school: row.school,
+            classes: row.classes,
+            division: row.division,
+            status: row.status,
+        };
+
+        setEditFormData(formValues);
+    }
+
+    // ReadOnly Mode Method
+    const handleDeleteClick = (studentId) => {
+        const newTableData = [...tableData];
+
+        const index = newTableData.findIndex((row) => row.id === studentId);
+
+        newTableData.splice(index, 1);
+
+        setTableData(newTableData);
+    }
+
+    // Edit Mode Method
+    const handleEditFormChange = (event) => {
+        event.preventDefault();
+
+        const { name, value } = event.target
+        setEditFormData({
+            ...editFormData,
+            [name]: value
+        })
+    }
+
+    // Edit Mode Method
+    const handleEditFormSubmit = (event) => {
+        event.preventDefault();
+
+        const editedFormData = {
+            id: editStudentId,
+            name: editFormData.name,
+            age: editFormData.age,
+            school: editFormData.school,
+            classes: editFormData.classes,
+            division: editFormData.division,
+            status: editFormData.status,
+        }
+
+        const newFormData = [...tableData];
+
+        const index = tableData.findIndex((data) => data.id === editStudentId);
+
+        newFormData[index] = editedFormData;
+
+        setTableData(newFormData);
+        setEditStudentId(null);
+    }
+
+    // Edit Mode Method
+    const handleCancelClick = () => {
+        setEditStudentId(null);
+    }
+
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
 
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
@@ -25,7 +111,7 @@ export default function ColumnGroupingTable() {
     };
 
     return (
-        <Paper sx={{ width: "100%" }}>
+        <form onSubmit={handleEditFormSubmit}>
             <TableContainer sx={{ maxHeight: 440 }}>
                 <Table aria-label="sticky table">
                     <TableHead>
@@ -43,42 +129,30 @@ export default function ColumnGroupingTable() {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {rows
-                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                        {tableData
+                            // .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage) 
                             .map((row) => {
                                 return (
-                                    <TableRow hover role="checkbox" tabIndex={-1} key={row.code} sx={{ '&:nth-of-type(odd)': { bgcolor: '#Fbe9e9' } }}>
-                                        {columns.map((column) => {
-                                            const value = row[column.id];
-                                            return (
-                                                <TableCell key={column.id} align={column.align}>
-                                                    {column.format && typeof value === "number"
-                                                        ? column.format(value)
-                                                        : value}
-                                                </TableCell>
-                                            );
-                                        })}
-                                        <TableCell sx={{ minWidth: 30, paddingRight: "0rem", textAlign: "right", textDecoration: "underline 2px #5db7e9", color: '#5db7e9' }}>
-                                            Edit
-                                        </TableCell>
-                                        <TableCell sx={{ minWidth: 30, paddingRight: "0rem", textAlign: "left", textDecoration: "underline 2px #5db7e9", color: '#5db7e9' }}>
-                                            Delete
-                                        </TableCell>
-                                    </TableRow>
+                                    <>
+                                        {editStudentId === row.id ?
+                                            <EditableRow row={row} editFormData={editFormData} handleEditFormChange={handleEditFormChange} handleEditFormSubmit={handleEditFormSubmit} handleCancelClick={handleCancelClick} key={row.id} />
+                                            : <ReadOnlyRow row={row} handleEditClick={handleEditClick} handleDeleteClick={handleDeleteClick} key={row.id} />
+                                        }
+                                    </>
                                 );
                             })}
                     </TableBody>
                 </Table>
             </TableContainer>
-            <TablePagination
-                rowsPerPageOptions={[5, 10]}
+            {/* <TablePagination
+                rowsPerPageOptions={[5, 10, { label: 'All', value: -1 }]}
                 component="div"
-                count={rows.length}
+                count={tableData.length}
                 rowsPerPage={rowsPerPage}
                 page={page}
                 onPageChange={handleChangePage}
                 onRowsPerPageChange={handleChangeRowsPerPage}
-            />
-        </Paper>
+            /> */}
+        </form>
     );
 }
